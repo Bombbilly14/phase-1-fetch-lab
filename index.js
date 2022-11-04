a2101f2de6fdc999ae06944780d83c0a9b0bcecf
William German <william.j.german2@gmail.com>
2022-11-04 09:58:22 -0700

    commit tree ef2d790702ccd1b2f544b1e5f7b15b3181b6b497

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then((res) => res.json())
+  .then(function (data) {
+    renderBooks(data)
+    
+
+  })
+  //renderBooks(res.json)
   
 }
+
+// .then((res) => res.json())
+//   .then((bookStore) => {
+//     console.log(bookStore);
+//     renderHeader(bookStore)
+//     renderFooter(bookStore)
 
 function renderBooks(books) {
   const main = document.querySelector('main');
